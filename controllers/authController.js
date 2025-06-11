37abfc51e2fc034bb2952ed2f2803bc2fccb19ac
Sahil7709 <sahiltheurkar3286@gmail.com>
2025-06-11 23:03:37 +0530

    final changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,12 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const generateToken = require('../utils/generateToken');
+const admin = require('firebase-admin');
+const serviceAccount = require('../serviceAccountKey.json');
 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
 
 exports.register = async (req, res) => {
     const { name, email, password } = req.body;
@@ -21,4 +26,23 @@ exports.login = async (req, res) => {
         return res.status(401).json({ message: 'Invalid credentials' });
 
     res.json({ token: generateToken(user._id) });
+};
+
+exports.googleLogin = async (req, res) => {
+    const { idToken } = req.body;
+
+    try {
+        const decodedToken = await admin.auth().verifyIdToken(idToken);
+        const { email, name } = decodedToken;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ email, name, isGoogleUser: true });
+        }
+
+        res.json({ token: generateToken(user._id) });
+    } catch (err) {
+        res.status(401).json({ message: 'Google Auth failed' });
+    }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const { register, login } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/google-login', googleLogin);
 
 module.exports = router;
